f97d32fc1f00250be95eec86fddf7b6853da99d0
Steven! <stevenragnarok@osrfoundation.org>
2017-03-09 11:04:41 -0800

    Attempt to set TinyXML_ROOT_DIR variable as well to work with tinyxml_vendor module.

--- package/tinyxml-usestl/share/cmake/TinyXMLConfig.cmake
+++ package/tinyxml-usestl/share/cmake/TinyXMLConfig.cmake
@@ -1,3 +1,4 @@
 set ( _PREFIX "$ENV{ChocolateyInstall}/lib/tinyxml-usestl")
+set (TinyXML_ROOT_DIR "${_PREFIX}")
 set (TinyXML_INCLUDE_DIRS "${_PREFIX}/include")
 set (TinyXML_LIBRARIES "${_PREFIX}/lib/tinyxmlSTL.lib")
