0f4a73f8e4f051b0fde9949f75d71d5b03c2837d
Steven! <stevenragnarok@osrfoundation.org>
2017-03-10 14:33:40 -0800

    Attempt to package both Debug and Release library versions.

--- package/tinyxml-usestl/share/cmake/TinyXMLConfig.cmake
+++ package/tinyxml-usestl/share/cmake/TinyXMLConfig.cmake
@@ -1,4 +1,4 @@
 set ( _PREFIX "$ENV{ChocolateyInstall}/lib/tinyxml-usestl")
 set (TinyXML_ROOT_DIR "${_PREFIX}")
 set (TinyXML_INCLUDE_DIRS "${_PREFIX}/include")
-set (TinyXML_LIBRARIES "${_PREFIX}/lib/tinyxml.lib")
+set (TinyXML_LIBRARIES debug "${_PREFIX}/lib.tinyxmld.lib" optimized "${_PREFIX}/lib/tinyxml.lib")
