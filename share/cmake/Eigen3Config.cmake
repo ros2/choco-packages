9c9daac0185a3859b5e0a1e52c569b1b5dc73095
Steven! <stevenragnarok@osrfoundation.org>
2017-03-13 10:43:02 -0700

    Change package name and include basic version config.
    The orocos_kdl project wasn't able to use the Chocolatey packaged Eigen because
    of issues with the package resolution. This renames the package in the CMake registry
    to "Eigen" from "Eigen3", which was originally taken from the FindEigen3.cmake module.
    
    It also sets a version config that will accept any version not less than 3.3.3.
    By local testing, not specifying minor and patch versions will compare correctly.
    That is, requesting version 3 will succeed.

--- share/cmake/Eigen3Config.cmake
+++
@@ -1,2 +0,0 @@
-set ( _PREFIX "$ENV{ChocolateyInstall}/lib/eigen")
-set (Eigen3_INCLUDE_DIRS "${_PREFIX}/include")
---
+++ share/cmake/EigenConfig.cmake
@@ -0,0 +1,4 @@
+set (_PREFIX "$ENV{ChocolateyInstall}/lib/eigen")
+set (Eigen_ROOT_DIR "${_PREFIX}")
+set (Eigen_INCLUDE_DIRS "${_PREFIX}/include")
+set (Eigen_INCLUDE_DIR "${Eigen_INCLUDE_DIRS}")
