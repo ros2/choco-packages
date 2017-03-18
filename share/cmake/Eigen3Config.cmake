abb85a74437a39813b2b21622af7ab2fd755e658
nobody <nobody@osrfoundation.org>
2017-03-17 17:19:53 -0700

    Set more variables for compatibility.
    Eigen upstream sets a ton of legacy variables. At some point we may need
    to set all of them or just use eigen's upstream cmake installer.
    To get our builds working, we need these two.
    
    This also fixes a typo in the previous legacy variable.

--- share/cmake/Eigen3Config.cmake
+++ share/cmake/Eigen3Config.cmake
@@ -1,3 +1,5 @@
 get_filename_component (_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 set (Eigen3_INCLUDE_DIRS "${_PREFIX}/include")
-set (Eigen3_INCLUDE_DIR "${Eigen_INCLUDE_DIRS}")
+# Compatibility with nonstandard and legacy package variable expectations.
+set (Eigen3_INCLUDE_DIR "${Eigen3_INCLUDE_DIRS}")
+set (EIGEN3_INCLUDE_DIRS "${Eigen3_INCLUDE_DIRS}")
