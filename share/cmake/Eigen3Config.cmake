4a09dd1a745c7a14347ccd69696c76264603cc33
Steven! <stevenragnarok@osrfoundation.org>
2017-03-20 14:07:15 -0700

    Define an additional variable for compatibility.

--- share/cmake/Eigen3Config.cmake
+++ share/cmake/Eigen3Config.cmake
@@ -3,3 +3,4 @@ set (Eigen3_INCLUDE_DIRS "${_PREFIX}/include")
 # Compatibility with nonstandard and legacy package variable expectations.
 set (Eigen3_INCLUDE_DIR "${Eigen3_INCLUDE_DIRS}")
 set (EIGEN3_INCLUDE_DIRS "${Eigen3_INCLUDE_DIRS}")
+set (EIGEN3_INCLUDE_DIR "${Eigen3_INCLUDE_DIRS}")
