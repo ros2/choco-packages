d4a45ebbb7c9a785a5722351127ffb4068937b32
Thomas Moulard <tmoulard@amazon.com>
2019-01-11 19:59:55 +0000

    Adds openssl 1.0.2 Chocolatey package
    Based on ROS 2 Source install documentation:
    https://index.ros.org/doc/ros2/Installation/Windows-Install-Binary/#install-openssl
    
    This package is a Chocolatey automatic package:
    https://chocolatey.org/docs/automatic-packages
    
    To build and install this package:
    
    $ cinst au
    $ cd packages/openssl
    $ ./update.ps1
    $ Get-RemoteFiles -Purge -NoSuffix
    $ choco pack
    $ cinst -s . openssl -y

---
+++ package/openssl/update.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module AU
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_GetLatest {
+  @{
+    URL32 = "https://slproweb.com/download/Win32OpenSSL-1_0_2q.exe"
+    URL64 = "https://slproweb.com/download/Win64OpenSSL-1_0_2q.exe"
+    Version = "1.0.2"
+    PackageName = 'OpenSSL'
+  }
+}
+
+update -ChecksumFor none
