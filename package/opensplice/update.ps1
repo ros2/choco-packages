8cb65f260890cfff29d7f95bbcc5a3d9237229b3
Thomas Moulard <tmoulard@amazon.com>
2019-01-12 02:36:42 +0000

    Adds OpenSplice 6.9.181127 package
    Based on ROS 2 Source install documentation:
    https://index.ros.org/doc/ros2/Installation/Windows-Install-Binary/#adlink-opensplice
    
    6.9.181127 is being packaged instead of 6.9.181126 because the
    OpenSplice release page does not curently contain this version.
    
    This package is a Chocolatey automatic package:
    https://chocolatey.org/docs/automatic-packages
    
    To build and install this package:
    $ cinst au
    $ cd packages/opensplice
    $ ./update.ps1
    $ Get-RemoteFiles -Purge -NoSuffix
    $ choco pack
    $ cinst -s . opensplice -y

---
+++ package/opensplice/update.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module AU
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_GetLatest {
+  @{
+    URL64 = "https://github.com/ADLINK-IST/opensplice/releases/download/OSPL_V6_9_181127OSS_RELEASE/PXXX-VortexOpenSplice-6.9.181127OSS-HDE-x86_64.win-vs2017-installer.zip"
+    Version = "6.9.181127"
+    PackageName = 'OpenSplice'
+  }
+}
+
+update -ChecksumFor none
