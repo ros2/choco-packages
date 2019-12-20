2d57ddcbf5495593c7cc3dfcad7c6ed2df12105e
ahcorde <ahcorde@gmail.com>
2019-12-20 12:22:52 +0100

    Add bullet package

---
+++ package/bullet/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# Source registry key values which are shared between install and uninstall
+. $PSScriptRoot\regKeys.ps1
+
+if (Test-Path $CMakeRegistryPath) {
+  if (Test-Path $CMakeSystemRepositoryPath) {
+      Remove-Item "$CMakeSystemRepositoryPath\$CMakePackageName"
+  }
+}
+
+# Removal of the dynamic library from the system Path
+# is handled automagically by the use of Install-ChocolateyPath.
